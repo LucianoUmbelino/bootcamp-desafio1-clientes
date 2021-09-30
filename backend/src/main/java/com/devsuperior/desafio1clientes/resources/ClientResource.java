913fafaac2d36d21fb19279ab537603b18587f01
Luciano Umbelino <luciano.umbelino@gmail.com>
2021-09-30 20:58:55 -0300

    ClientResource

---
+++ backend/src/main/java/com/devsuperior/desafio1clientes/resources/ClientResource.java
@@ -0,0 +1,26 @@
+package com.devsuperior.desafio1clientes.resources;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.desafio1clientes.entities.Client;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+	
+	@GetMapping
+	public ResponseEntity<List<Client>> findAll() {
+		List<Client> list = new ArrayList<>();
+		list.add(new Client(1L, "Maria Silva", "12345678901", 6500.0D, Instant.parse("1994-07-20T10:30:00Z"), 2));
+		list.add(new Client(2L, "Maria Silva", "12345678901", 6500.0D, Instant.parse("1994-07-20T10:30:00Z"), 2));
+		return ResponseEntity.ok().body(list);
+	}
+
+}
