ab58d83596b6b99da5e0b260fb33f7b9e9b3a1d9
Luciano Umbelino <luciano.umbelino@gmail.com>
2021-09-30 15:23:42 -0300

    Backend project desafio1-clientes created

---
+++ backend/src/main/java/com/devsuperior/desafio1clientes/Desafio1ClientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio1clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio1ClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio1ClientesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/desafio1clientes/Desafio1ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio1clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio1ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
