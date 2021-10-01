72b274454aba93b26af68c2cab75894e46f4969a
Luciano Umbelino <luciano.umbelino@gmail.com>
2021-10-01 20:59:43 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/desafio1clientes/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/desafio1clientes/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.desafio1clientes.entities.Client;
 import com.devsuperior.desafio1clientes.repositories.ClientRepository;
@@ -14,8 +15,8 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	public List<Client> findAll() {
-		
+	@Transactional(readOnly = true)
+	public List<Client> findAll() {		
 		return repository.findAll();
 		
 	}
